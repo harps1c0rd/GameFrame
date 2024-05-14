c63c929dc48d5c65aaea93663d85a4768da7608b
harps1c0rd <158243927+harps1c0rd@users.noreply.github.com>
2024-05-15 00:08:30 +1000

    net move

--- Objects/Net.py
+++ Objects/Net.py
@@ -1,4 +1,4 @@
-from GameFrame import RoomObject
+from GameFrame import RoomObject, Globals
 import random
 
 class Net(RoomObject):
@@ -20,3 +20,29 @@ class Net(RoomObject):
         # set travel direction
         angle = random.randint(135,225)
         self.set_direction(angle, 10)
+
+    def step(self):
+        """
+        Determines what happens to the asteroid on each tick of the game clock
+        """
+        self.keep_in_room()
+        self.outside_of_room()
+        
+    def keep_in_room(self):
+        """
+        Keeps the asteroid inside the top and bottom room limits
+        """
+        if self.y < 0:
+            self.y = 0
+            self.y_speed *= -1
+        elif self.y > Globals.SCREEN_HEIGHT - self.height:
+            self.y = Globals.SCREEN_HEIGHT - self.height
+            self.y_speed *= -1
+
+    def outside_of_room(self):
+        """
+        removes asteroid that have exited the room
+        """
+        if self.x + self.width < 0:
+            print("net deleted")
+            self.room.delete_object(self)
